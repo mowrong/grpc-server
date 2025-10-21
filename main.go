c302f4d22ded8a2cdb008685352311e454deed1f
pengqiang <pengqiang_1999@163.com>
2025-10-21 11:44:43 +0800

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/mowrong/grpc-api/github.com/mowrong/grpc-api/hello"
+	"google.golang.org/grpc"
+)
+
+type greeterServer struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *greeterServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	msg := fmt.Sprintf("Hello, %s!", req.GetName())
+	return &pb.HelloReply{Message: msg}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreeterServer(grpcServer, &greeterServer{})
+
+	log.Println("✅ gRPC server is running on :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
